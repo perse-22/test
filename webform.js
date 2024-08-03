f52726c16dd4fcfa32b33edf1a8f540870df3fb4
perse-22 <perseef5@gmail.com>
2024-08-03 13:47:43 +0800

    Create webform.js

---
+++ webform.js
@@ -0,0 +1,15 @@
+function handleSubmit() {
+    // Get form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    // Validate form values
+    if (name && email) {
+        // Display form data
+        document.getElementById("formResult").innerHTML = 
+            "<strong>Form submitted successfully!</strong><br>" +
+            "Name: " + name + "<br>" +
+            "Email: " + email;
+    } else {
+        document.getElementById("formResult").innerText = "Please fill out all fields.";
+    }
