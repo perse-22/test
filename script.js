3283cd2ab1ca5aa3324b6daff1efc18a7d7c246d
perse-22 <perseef5@gmail.com>
2024-08-03 12:57:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 function sayHello() {
     alert("Hello, World!");
+}
+
+function showSum(a, b) {
+    alert("The sum of " + a + " and " + b + " is " + (a + b));
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = "lightblue";
+}
