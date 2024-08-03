820b7f9c5277a793846098c93c55c3b08c413521
perse-22 <perseef5@gmail.com>
2024-08-03 13:13:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 function sayHello() {
-    document.getElementById("message").innerText = "Hello, World!";
+    document.getElementById("formResult").innerText = "Hello, World!";
 }
 
 function showSum(a, b) {
-    document.getElementById("message").innerText = "The sum of " + a + " and " + b + " is " + (a + b);
+    document.getElementById("formResult").innerText = "The sum of " + a + " and " + b + " is " + (a + b);
 }
 
 function changeBackgroundColor() {
     document.body.style.backgroundColor = "lightblue";
-    document.getElementById("message").innerText = "Background color changed to light blue!";
+    document.getElementById("formResult").innerText = "Background color changed to light blue!";
+}
+
+function handleSubmit() {
+    // Get form values
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+
+    // Validate form values
+    if (name && email) {
+        document.getElementById("formResult").innerText = "Form submitted successfully! Name: " + name + ", Email: " + email;
+    } else {
+        document.getElementById("formResult").innerText = "Please fill out all fields.";
+    }
 }
