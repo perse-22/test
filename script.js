76e9cba79564d9f55d0d18ae760d0f7542f755bf
perse-22 <perseef5@gmail.com>
2024-08-03 13:48:36 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,19 +11,4 @@ function changeBackgroundColor() {
     document.getElementById("formResult").innerText = "Background color changed to light blue!";
 }
 
-function handleSubmit() {
-    // Get form values
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
 
-    // Validate form values
-    if (name && email) {
-        // Display form data
-        document.getElementById("formResult").innerHTML = 
-            "<strong>Form submitted successfully!</strong><br>" +
-            "Name: " + name + "<br>" +
-            "Email: " + email;
-    } else {
-        document.getElementById("formResult").innerText = "Please fill out all fields.";
-    }
-}
